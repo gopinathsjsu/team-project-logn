3f242fbbb464d066450a4b5f987200249bd7f34a
Kaushal K R <kaushalkr27@gmail.com>
2022-05-12 15:41:52 -0700

    login and signup pages completed.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="pt-2 container h-16 flex justify-between items-center">
       <div>LOGO</div>
       <div>
-        <button className="mx-4 bg-button-color text-white px-8 py-2 rounded-full transition-all hover:shadow-button-shadow">
-          Sign In
-        </button>
-        <button className="bg-button-color text-white px-8 py-2 rounded-full transition-all hover:shadow-button-shadow">
-          Sign Up
-        </button>
+        <Link href="/login">
+          <button className="mx-4 bg-button-color text-white px-8 py-2 rounded-full transition-all hover:shadow-button-shadow">
+            Sign In
+          </button>
+        </Link>
+        <Link href="/signup">
+          <button className="bg-button-color text-white px-8 py-2 rounded-full transition-all hover:shadow-button-shadow">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
