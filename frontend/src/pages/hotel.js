0f238b681632cd0d4894b034d2e26c1ee7320543
shravya-k <k.shravya96@gmail.com>
2022-05-14 19:18:27 -0700

    hotel admin view

---
+++ frontend/src/pages/hotel.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Hotel() {
+  const [type, setType] = useState("in");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  return (
+    <div className="container">
+      <div>
+        <Link href="/hotelLogin">
+        <button
+          className="mr-4 bg-button-color text-white px-8 py-2 rounded-full transition-all hover:shadow-button-shadow"
+        >
+          Hotel Sign In
+        </button>
+        </Link>
+        <Link href="/hotelSignup">
+        <button
+          className="bg-button-color text-white px-8 py-2 rounded-full transition-all hover:shadow-button-shadow"
+        >
+          Hotel Sign Up
+        </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
